54bd32531cba3279d962d542a7248ed4eb10ac70
Lollybot <90723391+lollybot@users.noreply.github.com>
2022-08-27 00:22:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("./app/app");
+const server = app.listen((process.env.PORT || 3000));
+
+process.on("SIGINT", () => {
+    if (process.env.NODE_ENV === "production") {
+        server.close(function(error) {
+            console.error("SIGINT received, shutting down");
+            if (error) {
+                console.error(err);
+                process.exit(1);
+            }
+        })
+    } else {
+        process.exit(0);
+    }
+})
